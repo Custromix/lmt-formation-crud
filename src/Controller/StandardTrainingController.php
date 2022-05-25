5bfcef5eff64c2826b33e69d3b21f426c9fa4655
Custromix <75646944+Custromix@users.noreply.github.com>
2022-05-25 20:26:08 +0200

    add other crud

--- src/Controller/StandardTrainingController.php
+++ src/Controller/StandardTrainingController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/standard/training")
+ * @Route("/standardtraining")
  */
 class StandardTrainingController extends AbstractController
 {
